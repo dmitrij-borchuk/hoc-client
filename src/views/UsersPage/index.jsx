7f56e3e4db664a9ee9bfad0059d6b6b2da0c74e7
Dmytro Borchuk <dborchuk@remit.se>
2018-06-03 21:02:28 +0300

    Add user

--- src/views/UsersPage/index.jsx
+++ src/views/UsersPage/index.jsx
@@ -4,17 +4,15 @@ import PropTypes from 'prop-types';
 import Button from 'material-ui/Button';
 import AddIcon from 'material-ui-icons/Add';
 import Snackbar from 'material-ui/Snackbar';
-import { reduxForm, Field, submit } from 'redux-form';
+import { submit } from 'redux-form';
 import * as usersActions from '../../actions/users';
 import { usersPageGetData } from '../../actions/pages';
 import List from '../../components/List';
 import Dialog from '../../components/DialogForm';
 import { Fab } from '../../commonStyles';
-import { renderTextField } from '../../utils';
 import { DialogFormBody } from '../../components/DialogForm/styles';
 import Loader from '../../components/Loader';
-
-const FORM_NAME = 'usersPage';
+import CreateUserForm, { FORM_NAME } from '../../components/CreateUserForm';
 
 class UsersPage extends PureComponent {
   static propTypes = {
@@ -28,7 +26,8 @@ class UsersPage extends PureComponent {
     listFetchingError: PropTypes.shape({
       status: PropTypes.number,
     }),
-    onSaveClick: PropTypes.func.isRequired,
+    saveUser: PropTypes.func.isRequired,
+    submitCreateUserForm: PropTypes.func.isRequired,
     creationFetching: PropTypes.bool.isRequired,
     dialogOpened: PropTypes.bool.isRequired,
     setUserDialogState: PropTypes.func.isRequired,
@@ -43,6 +42,16 @@ class UsersPage extends PureComponent {
     this.props.getData();
   }
 
+  saveUser = async (data) => {
+    const {
+      saveUser,
+      setUserDialogState,
+    } = this.props;
+
+    await saveUser(data);
+    setUserDialogState(false);
+  }
+
   render() {
     const {
       users,
@@ -51,17 +60,13 @@ class UsersPage extends PureComponent {
       creationErrors,
       creationFetching,
       dialogOpened,
-      onSaveClick,
       setUserDialogState,
+      submitCreateUserForm,
     } = this.props;
-    const items = users.map(user => user.username);
-    const errorMap = creationErrors.reduce(
-      (acc, cur) => ({
-        ...acc,
-        [cur.path]: cur,
-      }),
-      {},
-    );
+    const items = users.map(user => ({
+      text: user.username,
+      key: user.id.toString(),
+    }));
 
     if (listFetching) {
       return <Loader />;
@@ -87,7 +92,9 @@ class UsersPage extends PureComponent {
 
     return (
       <Fragment>
-        <List items={items} />
+        <List
+          items={items}
+        />
         <Fab>
           <Button
             variant="fab"
@@ -102,27 +109,14 @@ class UsersPage extends PureComponent {
         <Dialog
           isOpened={dialogOpened}
           onClose={() => setUserDialogState(false)}
-          onSave={() => onSaveClick()}
+          onSave={submitCreateUserForm}
           title="New user"
         >
           <DialogFormBody>
-            <Field
-              name="email"
-              component={renderTextField}
-              error={!!errorMap.email}
-              helperText={errorMap.email && errorMap.email.message}
-              label="Email"
-              fullWidth
+            <CreateUserForm
               disabled={creationFetching}
-            />
-            <Field
-              name="username"
-              component={renderTextField}
-              error={!!errorMap.username}
-              helperText={errorMap.username && errorMap.username.message}
-              label="Username"
-              fullWidth
-              disabled={creationFetching}
+              onSubmit={this.saveUser}
+              errors={creationErrors}
             />
           </DialogFormBody>
           {creationFetching && <Loader />}
@@ -143,14 +137,12 @@ const mapStateToProps = ({ users, pages }) => ({
 
 const mapDispatchToProps = dispatch => ({
   getData: () => dispatch(usersPageGetData()),
-  onSaveClick: () => dispatch(submit(FORM_NAME)),
-  onSubmit: data => dispatch(usersActions.createUser(data)),
+  submitCreateUserForm: () => dispatch(submit(FORM_NAME)),
+  saveUser: data => dispatch(usersActions.createUser(data)),
   setUserDialogState: data => dispatch(usersActions.setUserDialogState(data)),
 });
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(reduxForm({
-  form: FORM_NAME,
-})(UsersPage));
+)(UsersPage);
